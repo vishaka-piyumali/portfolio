c53eb42c8e8bdbe995fc21f2b924db7c7747cd46
Vishaka P Wele Uhulanage <vishaka.piyumali@optus.com.au>
2018-06-06 10:56:27 +1000

    fixing watch tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -16,28 +16,33 @@ const pmRoot = 'node_modules';
 
 gulp.task('ractive-templates', function() {
 	return gulp.src('src/**/*.hbs')
-		.pipe(gulpRactive())
-		.pipe(declare({
-			namespace: 'Templates',
-			noRedeclare: true
-		}))
-		.pipe(concat('templates.js'))
-		.pipe(wrap({
-			exports: 'this.Templates'
-		}))
-		.pipe(uglify())
-		.pipe(gulp.dest('./public/js/'));
+			.pipe(gulpRactive())
+			.pipe(declare({
+				namespace: 'Templates',
+				noRedeclare: true
+			}))
+			.pipe(concat('templates.js'))
+			.pipe(wrap({
+				exports: 'this.Templates'
+			}))
+			.pipe(uglify())
+			.pipe(gulp.dest('./public/js/'));
 });
 
-gulp.task('lib-js', function() {
+gulp.task('lib-js', ['npm-js'],  function() {
+	return gulp.src(['src/lib/vendors/js/**/*.js'],{ base: 'src/lib/vendors/js'})
+			.pipe(gulp.dest('public/js/vendors'))
+});
+
+gulp.task('npm-js', function() {
 	return gulp.src(['ractive/ractive.js',
-			'jquery/dist/jquery.js',
-			'foundation-sites/dist/js/foundation.min.js',
-			'requirejs/require.js',
-			'lodash-compat/index.js'
-		], {cwd: pmRoot, base: pmRoot})  // Get source files with gulp.src
-		.pipe(uglify())
-		.pipe(gulp.dest('public/js/vendors'))
+		'jquery/dist/jquery.js',
+		'foundation-sites/dist/js/foundation.min.js',
+		'requirejs/require.js',
+		'lodash-compat/index.js'
+	], {cwd: pmRoot, base: pmRoot})
+			.pipe(uglify())
+			.pipe(gulp.dest('public/js/vendors'))
 });
 
 gulp.task('components-js', function() {
@@ -50,65 +55,73 @@ gulp.task('components-js', function() {
 	};
 
 	return gulp.src('src/framework/components/**/*.js') // Get source files with gulp.src
-		.pipe(babel())
-		.pipe(concat('components.js'))
-		.pipe(wrap({
-			deps: _.keys(depsParamsMap),
-			params: _.values(depsParamsMap),
-			exports: 'Ractive.components'
-		}))
-		.pipe(uglify())
-		.pipe(gulp.dest('public/js'))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
+			.pipe(babel())
+			.pipe(concat('components.js'))
+			.pipe(wrap({
+				deps: _.keys(depsParamsMap),
+				params: _.values(depsParamsMap),
+				exports: 'Ractive.components'
+			}))
+			.pipe(uglify())
+			.pipe(gulp.dest('public/js'))
+			.pipe(browserSync.reload({
+				stream: true
+			}))
 });
 
 gulp.task('js', ['components-js', 'lib-js'], function() {
 	return gulp.src('src/framework/*.js') // Get source files with gulp.src
-		.pipe(babel())
-		.pipe(uglify())
-		.pipe(gulp.dest('public/js'))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
+			.pipe(babel())
+			.pipe(uglify())
+			.pipe(gulp.dest('public/js'))
+			.pipe(browserSync.reload({
+				stream: true
+			}))
 });
 
 
-gulp.task('sass', ['foundation'], function() {
+gulp.task('sass', function() {
 	return gulp.src('src/scss/**/*.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('public/css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
+			.pipe(sass())
+			.pipe(gulp.dest('public/css'))
+			.pipe(browserSync.reload({
+				stream: true
+			}))
 });
 
 gulp.task('foundation', function() {
 	return gulp.src('src/lib/vendors/css/foundation.6.4.scss')
-		.pipe(sass(
-				{includePaths: ['node_modules/foundation-sites/scss']}
-		))
-		.pipe(gulp.dest('public/css/vendors'))
+			.pipe(sass(
+					{includePaths: ['node_modules/foundation-sites/scss']}
+			))
+			.pipe(gulp.dest('public/css/vendors'))
 });
+
+gulp.task('lib-css', function() {
+	return gulp.src(['src/lib/vendors/css/kudos/*.css'])
+	//.pipe(sass())
+			.pipe(gulp.dest('public/css/vendors'))
+});
+
+
 
 gulp.task('clean', function() {
 	return del.sync('public');
 });
 
 gulp.task('assets', function() {
-	return gulp.src('./src/assets/**/*.*') // Get source files with gulp.src
+	return gulp.src('./src/assets/**/*.*')
 			.pipe(gulp.dest('public/assets'))
 });
 
 gulp.task('data', function() {
-	return gulp.src('src/data/*.json') // Get source files with gulp.src
+	return gulp.src('src/data/*.json')
 			.pipe(gulp.dest('public/data'))
 });
 
 gulp.task('html', function() {
-	return gulp.src('src/**/*.html') // Get source files with gulp.src
-		.pipe(gulp.dest('public/'))
+	return gulp.src('src/**/*.html')
+			.pipe(gulp.dest('public/'))
 });
 
 gulp.task('browserSync', function() {
@@ -131,10 +144,10 @@ gulp.task('watch', function(){
 	gulp.watch('src/scss/**/*.scss', ['sass']);
 	gulp.watch('src/js/**/*.js', ['js']);
 	gulp.watch('src/**/*.html', ['html']);
-	gulp.watch('src/**.*.hbs', ['ractive-templates'])
+	gulp.watch('src/**/*.hbs', ['ractive-templates'])
 });
 
 
-gulp.task('start', ['clean', 'assets', 'data', 'sass', 'ractive-templates', 'js', 'html', 'server', 'watch'], function (){
+gulp.task('default', ['clean', 'assets', 'data', 'lib-css', 'foundation', 'sass', 'ractive-templates', 'js', 'html', 'server', 'watch'], function (){
 	// ...
 });
